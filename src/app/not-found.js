776ff36d1952b6d957e616219fc37e77d81d8618
ahsan <ahsan.naveed1999@gmail.com>
2025-06-09 04:26:19 +0500

    not found fixed

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     return (
         <div className="text-center py-10">
             <h1 className="text-2xl font-bold mb-4">404 - Page Not Found</h1>
-            <p>The page you're looking for doesn't exist.</p>
+            <p>The page you&apos;re looking for doesn&apos;t exist.</p>
         </div>
     );
 }
